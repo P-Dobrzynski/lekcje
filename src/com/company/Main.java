7fec96718ab1717c758f937ae42a4cf10cc92265
Piotr Dobrzyński <piotr.dobrzynski@gmail.com>
2019-12-30 17:06:18 +0100

    dodałem komentarz

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,11 @@
 package com.company;
 
 public class Main {
+// tak się robi komentarz
+
+    /* tak kilko
+    linijkowy
+     */
 
     public static void main(String[] args) {
         System.out.println("Hello!");
