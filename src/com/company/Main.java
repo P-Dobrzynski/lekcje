6cfc817ee2ae3bdc4e4b722f0d30fa26e1a49ff5
Piotr Dobrzyński <piotr.dobrzynski@gmail.com>
2019-12-30 17:58:43 +0100

    dodane zmiany

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,34 @@
 package com.company;
 
+import javax.print.attribute.standard.PDLOverrideSupported;
+
 public class Main {
+// tak się robi komentarz
+
+    /* tak kilko
+    linijkowy
+     */
 
     public static void main(String[] args) {
-        System.out.println("Hello!");
+        String name = "John ";
+        String LastName = "Wick";
+        int myNumber = 8;
+        char myLetter = 'D';
+        boolean myTrouth = true;
+        double myDoubleNumber = 1.99;
+        float myFloutNumber = 2.99f;
 
+
+        System.out.println("Hello!");
+        System.out.println(name);
+        System.out.println(myNumber);
+        System.out.println(myLetter);
+        System.out.println(myDoubleNumber);
+        System.out.println(myTrouth);
+        System.out.println(name + LastName);
+
+        int x = 8;
+        int y= 6;
+        System.out.println(x + y);
     }
 }
---
+++ src/com/company/test.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class test {
+
+    public static void main(String[] args) {
+        // rozszerzanie i zwężanie
+        int myInt=9;
+        double myDouble=myInt;
+
+        System.out.println(myDouble);
+
+
+        double myDouble1=9.93;
+        int myInt1=(int) myDouble1;
+
+        System.out.println(myDouble1);
+    }
+}
