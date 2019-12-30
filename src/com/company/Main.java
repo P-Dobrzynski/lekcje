8202d95d4415e4dd044ca857758ce6543ae960a4
Piotr Dobrzyński <piotr.dobrzynski@gmail.com>
2019-12-30 19:23:02 +0100

    dodane else statement i switche

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -48,5 +48,49 @@ public class Main {
 
 // matma
         System.out.println(Math.random());
+
+// if else else if statement
+        int time = 22;
+        if (time <10) {
+            System.out.println("good morning");
+        } else if (time <20) {
+            System.out.println("good day");
+        } else {
+            System.out.println("Good evening");
+        }
+
+// switche
+        int day = 7;
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Fridayt");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("Looking forward for the weekend");
+        }
+
+//loops
+        
+
+
+        }
     }
-}
+
